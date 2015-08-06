911696b012117fb8d14d7dcb787f2e8978cf156b
Chessil Sojan <git@chessilsojan@gmail.com>
2015-08-06 17:44:05 +1200

    Commit -Ches- 1

--- src/Calcs.java
+++ src/Calcs.java
@@ -8,6 +8,7 @@ public class Calcs
     static double num2;
     static double ans;
           //this is a sample pgm
+    //this is a sample pgm
     public static void main(String[] args) {
     do{
     System.out.println("1 . Addition 2.Substraction 3.Multiplication 4.Division");
